49cc8a985d7c5dba09b3859283f52eb38e3c4e30
odekyc <odekyc@gmail.com>
2018-06-02 12:36:31 -0700

    hi

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php //Get Heroku ClearDB connection information
+    $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server   = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db       = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    $db['default'] = array(
+        'dsn'   => '',
+        'hostname' => 'us-cdbr-iron-east-05.cleardb.net',
+        'username' => 'b1a21033157577',
+        'password' => 'b2cfecca',
+        'database' => 'heroku_537f097b523902b',
+        'dbdriver' => 'mysqli',
+        'dbprefix' => '',
+        'pconnect' => FALSE,
+        'db_debug' => (ENVIRONMENT !== 'production'),
+        'cache_on' => FALSE,
+        'cachedir' => '',
+        'char_set' => 'utf8',
+        'dbcollat' => 'utf8_general_ci',
+        'swap_pre' => '',
+        'encrypt' => FALSE,
+        'compress' => FALSE,
+        'stricton' => FALSE,
+        'failover' => array(),
+        'save_queries' => TRUE
+    );
